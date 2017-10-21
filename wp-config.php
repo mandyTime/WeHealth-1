35fcc827a270f54173c0483a2f34f5af4e8d6995
mandyTime <540042281@qq.com>
2017-10-21 22:41:18 +0800

    升级wordpress，配置忽略配置项

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'weHealth');
+define('DB_NAME', 'wehealth');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'nishizhu1989');
+define('DB_PASSWORD', '');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
